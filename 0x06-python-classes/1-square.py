f405f6aaad477ed70ec2be13d98dab9339dff937
Salma-Engmelb <docmel2409@gmail.com>
2023-12-22 03:53:20 -0800

    second task

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Square module"""
+
+class Square:
+    """Defines a square"""
+
+    def __init___(self, size):
+        """Initialize a new Square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        self.__size = size
